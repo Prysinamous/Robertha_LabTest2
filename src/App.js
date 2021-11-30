58d5110032d520f856398a40a4d852e0d70fa848
Prysinamous <74151231+Prysinamous@users.noreply.github.com>
2021-11-30 17:33:24 -0500

    almost done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import Labtest2 from './Weather'
+import './pretty.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Robertha Alvarez-101236645 (Lab Test 2)</h1>
+      <Labtest2/>
+      
     </div>
   );
 }
 
 export default App;
+
+
---
+++ src/Weather.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import axios from 'axios'
+
+export default function Labtest2() {
+
+const api=
+{
+    key: "810f2c4c204a53b18da25521453ef5d1",
+    base: "https://api.openweathermap.org/data/2.5/"
+}
+
+let 
+
+    return (
+        <div>
+            <div className="search">
+            <input
+            type= "text"
+            className="searching"
+            placeholder="Search or else!"
+            >    
+            </input>
+            </div>
+
+            <div className='city'>
+            <h1>Toronto, Ontario</h1>
+            <h1>Date is: 2021-11-30</h1>
+
+            </div>
+        </div>
+
+
+
+    )
+}
