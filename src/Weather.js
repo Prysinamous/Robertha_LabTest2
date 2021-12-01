3b0b86a380a2d04ea9d7f23bc558ddd281fbc076
Robertha Alvarez Diaz <74151231+Prysinamous@users.noreply.github.com>
2021-11-30 19:56:08 -0500

    Add files via upload

--- src/Weather.js
+++ src/Weather.js
@@ -1,15 +1,31 @@
 import React from 'react'
+import { useState } from 'react';
 import axios from 'axios'
 
 export default function Labtest2() {
 
 const api=
 {
-    key: "810f2c4c204a53b18da25521453ef5d1",
+
+    key: "415534b2a698b17b42f72b14efc39453",
     base: "https://api.openweathermap.org/data/2.5/"
 }
 
-let 
+const[get,setGet]= useState('');
+const[weather,setWeather]= useState('');
+
+const f = function myFunction()
+{
+    fetch(`${api.base}weather?q=${get}&units=metric&APPID=${api.key}`)
+
+        .then(res => res.json())
+    .then(result => {
+        setWeather(result);
+        setGet('');
+    });
+}
+
+    console.log(weather)
 
     return (
         <div>
@@ -17,19 +33,35 @@ let
             <input
             type= "text"
             className="searching"
-            placeholder="Search or else!"
+            placeholder="Search......."
+            onChange={change => setGet(change.target.value)}
+            value={get}
+            onKeyPress={f}
             >    
             </input>
             </div>
 
             <div className='city'>
-            <h1>Toronto, Ontario</h1>
+            <h1>{weather}</h1>
             <h1>Date is: 2021-11-30</h1>
+            </div>
+            
+            <div className= 'weather_summary'>
+            <h1>{weather.main}</h1>
+             <div className= 'temp'>
+                 -50°
+             </div>
+            <div className='weather'>
+                Freezing Cold
+            </div>
+
+                <div className="square">
+                        .
+                </div>
+
 
             </div>
         </div>
 
-
-
-    )
+    );
 }
